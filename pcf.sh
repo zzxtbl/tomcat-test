6d0143d869bf8045625e912810e28fbe28f99727
aravind-mohanraj <55225520+aravind-mohanraj@users.noreply.github.com>
2020-05-27 21:59:25 +0530

    Delete pcf.sh

--- pcf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cf login -a https://api.run.pivotal.io/ -u arvind.mohanraj8@gmail.com -p Ansible@2020 -o hardy-test -s development
-cd /opt/bamboo-home/xml-data/build-dir/TOM-TOM-JOB1
-cf push
